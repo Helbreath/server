9bd9186f06df65226e81a7b90ecc6753fbfdd003
Sharon Fox <sharon@sharonfox.dev>
2021-01-09 06:17:49 -0500

    gserver responsds to enter game request

--- src/gserver.cpp
+++ src/gserver.cpp
@@ -68,6 +68,11 @@ void gserver::handle_new_client(std::shared_ptr<client> _client)
 {
     std::unique_lock<std::mutex> l(cl_m);
     clients.insert(_client);
+
+    stream_write sw;
+    sw.write_enum(log_rsp_message_id::enter_game);
+    sw.write_enum(enter_game_msg::CONFIRM);
+    _client->write(sw);
 }
 
 void gserver::handle_close_client(std::shared_ptr<client> _client)
